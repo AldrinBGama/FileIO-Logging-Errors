9a9af028fcaeef64307b63ba326d16423f94a8cc
aldrinbgama <aldrin.gama@umuzi.org>
2020-01-09 13:07:28 +0200

    1st commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input;
+        do {
+            Write write = new Write();
+            write.save();
+            System.out.println("Type Yes to Add more Visitors or No to exit");
+            input=s.nextLine();
+        }while (input.equalsIgnoreCase("yes"));
+
+    }
+}
---
+++ src/main/java/Write.java
@@ -0,0 +1,42 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Write {
+    String name;
+    String surname;
+    int age;
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Date date = new Date();
+    Scanner scanner = new Scanner(System.in);
+
+
+    public void save(){
+            System.out.println("Enter Name");
+            name =scanner.nextLine();
+            System.out.println("Enter Surname");
+            surname =scanner.nextLine();
+            System.out.println("Enter Age");
+            age= scanner.nextInt();
+            System.out.println("File Saved");
+
+        while (true){
+            FileWriter fileWriter = null;
+            try {
+                fileWriter = new FileWriter("visitor_"+name+"_"+surname+".txt");
+                fileWriter.write("Full name: "+name+" "+surname + "\n" + "Age: "+Integer.toString(age) + "\n" +"Date of visit: "
+                        +sdf.format(date));
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+    }
+}
